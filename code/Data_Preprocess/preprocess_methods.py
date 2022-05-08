22f93661a55f7afd46fd127136a7c4e60e6abfee
Manlin-Li <mli59@u.rochester.edu>
2022-05-08 14:47:42 -0700

    add: binarization and boosted performance

--- code/Data_Preprocess/preprocess_methods.py
+++ code/Data_Preprocess/preprocess_methods.py
@@ -1,14 +1,14 @@
 import pandas as pd
 
 # To binarize `adata_df_preprocessed.csv`
-# def binarize(df):
-#     df_cate = df.select_dtypes(include=['O'])
-#     df = df.select_dtypes(exclude=['O'])
+def binarize(df):
+    num_cols = list(df.select_dtypes(exclude=['O']).columns)
+    df[num_cols] = (df[num_cols] > 0.0).astype(int)
 
+    return df
 
 def exclude_cate(df):
     return df.select_dtypes(exclude=['O'])
-
 
 def onehot_encoding(df):
     cat_ftrs = list(df.select_dtypes(include=['O']).columns)
--- code/main.py
+++ code/main.py
@@ -8,12 +8,16 @@ from loss_visualization import visualize_plots
 
 
 def main():
-    train_X, test_X, train_y, test_y = Data_Preprocess.load_train_and_test("../data/adata_df_2k_grouped_without_cate",
+    # train_X, test_X, train_y, test_y = Data_Preprocess.load_train_and_test("../data/adata_df_2k_grouped_one_hot_cate_binarized",
+    #                                                                        "../data/adata_df_2k_grouped.csv",
+    #                                                                        Data_Preprocess.onehot_encoding, Data_Preprocess.binarize)
+
+    train_X, test_X, train_y, test_y = Data_Preprocess.load_train_and_test("../data/adata_df_2k_grouped_without_cate_binarized",
                                                                            "../data/adata_df_2k_grouped.csv",
-                                                                           Data_Preprocess.exclude_cate)
+                                                                           Data_Preprocess.exclude_cate, Data_Preprocess.binarize)
 
     ##################baseline MLP#####################
-    # baseline_model = Model.Baseline_MLP(feature_nums=[100, 50, 25], dropout_rate=0.25)
+    # baseline_model = Model.Baseline_MLP(feature_nums=[160, 50, 25], dropout_rate=0.25)
     #
     # baseline_model.compile(optimizer=tf.keras.optimizers.Adam(learning_rate=0.0002),
     #                        loss=tf.keras.losses.BinaryCrossentropy(),
@@ -26,37 +30,37 @@ def main():
     #     batch_size=100,
     #     verbose=2
     # )
-
+    #
     # testing_result = baseline_model.evaluate(test_X, test_y, verbose=0)
     # visualize_plots(history)
     # print(dict(zip(baseline_model.metrics_names, testing_result)))
 
     ##################PCA#####################
-    # pca = PCA(n_components = 100)
-    # pca_train_X = pca.fit_transform(train_X)
-    # pca_test_X = pca.transform(test_X)
-    #
-    # pca_model = Model.Baseline_MLP(feature_nums=[200, 50, 25])
-    # pca_model.compile(optimizer=tf.keras.optimizers.Adam(learning_rate=0.001),
-    #                   loss=tf.keras.losses.BinaryCrossentropy(),
-    #                   metrics=[tf.keras.metrics.BinaryAccuracy(), tf.keras.metrics.AUC()])
-    #
-    # history = pca_model.fit(
-    #     pca_train_X, train_y,
-    #     validation_data=(pca_test_X, test_y),
-    #     epochs=1000,
-    #     batch_size=500,
-    #     verbose=2,
-    # )
-    # pca_testing_result = pca_model.evaluate(pca_test_X, test_y, verbose=2)
-    # visualize_plots(history)
-    # print(dict(zip(pca_model.metrics_names, pca_testing_result)))
-    # print(np.sum(test_y)/len(test_y))
+    pca = PCA(n_components=100)
+    pca_train_X = pca.fit_transform(train_X)
+    pca_test_X = pca.transform(test_X)
+
+    pca_model = Model.Baseline_MLP(feature_nums=[64, 32, 16, 8])
+    pca_model.compile(optimizer=tf.keras.optimizers.Adam(learning_rate=0.001),
+                      loss=tf.keras.losses.BinaryCrossentropy(),
+                      metrics=[tf.keras.metrics.BinaryAccuracy(), tf.keras.metrics.AUC()])
+
+    history = pca_model.fit(
+        pca_train_X, train_y,
+        validation_data=(pca_test_X, test_y),
+        epochs=100,
+        batch_size=1000,
+        verbose=2,
+    )
+    pca_testing_result = pca_model.evaluate(pca_test_X, test_y, verbose=2)
+    visualize_plots(history)
+    print(dict(zip(pca_model.metrics_names, pca_testing_result)))
+    print(np.sum(test_y)/len(test_y))
 
     ##################XGBOOST#####################
-    xgb = Model.Baseline_XGB(need_train=True)  # once trained, set `need_train` to false to let model load local parameters
-    xgb.train(train_X, train_y)
-    xgb.test(test_X, test_y)
+    # xgb = Model.Baseline_XGB(need_train=True)  # once trained, set `need_train` to false to let model load local parameters
+    # xgb.train(train_X, train_y)
+    # xgb.test(test_X, test_y)
 
 
 if __name__ == '__main__':
