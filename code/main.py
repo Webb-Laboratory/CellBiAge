560718847040cb5f51a50cf5c8b874f6edb4aa40
Manlin-Li <mli59@u.rochester.edu>
2022-05-10 18:21:16 -0700

    Added: binarization according to median

--- code/main.py
+++ code/main.py
@@ -8,12 +8,12 @@ def main():
     save_dir = "../results"
 
     # define dataset
-    train_X, test_X, train_y, test_y, dataset_name = one_hot_binarized_dataset()
+    train_X, test_X, train_y, test_y, dataset_name = one_hot_binarized_origin_dataset()
     # train_X, test_X, train_y, test_y, dataset_name = PCA_dataset(without_cate_binarized_dataset, n_components=100)    # if want PCA
 
     # define train parameters
     parameters = train_params(
-        learning_rate=0.0001,
+        learning_rate=0.001,
         loss=tf.keras.losses.BinaryCrossentropy(),
         metrics=[tf.keras.metrics.BinaryAccuracy(), tf.keras.metrics.AUC()],
         epochs=100,
@@ -21,8 +21,8 @@ def main():
     )
 
     # define your model
-    model = Model.Baseline_MLP(feature_nums=[160, 50, 25], dropout_rate=0.25)
-    model_name = "MLP_layer_160_50_25_with_binarized"   # !!!!!! Be sure to change it every time, or your local record will be overwritten
+    model = Model.Baseline_MLP(feature_nums=[100, 50, 10], dropout_rate=0.25)
+    model_name = "MLP_layer_100_50_10_lr_001_with_cate_bin1888"   # !!!!!! Be sure to change it every time, or your local record will be overwritten
 
     # Start Training, after trainning, all records will be dumped to the `save_dir/model_name` dir
     train(os.path.join(save_dir, model_name), model, parameters, train_X, train_y, test_X, test_y, dataset_name)
